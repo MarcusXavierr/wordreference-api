0785f0cc3193633b1f5557fa902c83c541a4ff75
Marcus Xavier <marcus.xavier@appmax.com.br>
2023-02-13 22:54:45 -0300

    refactor(parser): refactoring tests

--- pkg/scraper/scraper_test.go
+++ pkg/scraper/scraper_test.go
@@ -9,22 +9,11 @@ import (
 
 	"github.com/MarcusXavierr/wordreference-api/pkg/scraper"
 	"github.com/antchfx/htmlquery"
+	"golang.org/x/net/html"
 )
 
 func TestReadPageContent(t *testing.T) {
-	filename := "dumb_down_definition.html"
-	path := filepath.Join("testdata", filename)
-
-	content, err := ioutil.ReadFile(path)
-
-	if err != nil {
-		t.Fatalf("error reading testfile %s: %s", filename, err)
-	}
-
-	html, err := htmlquery.Parse(strings.NewReader(string(content)))
-	if err != nil {
-		t.Fatal("could not open the file")
-	}
+	html := getParsedHTML(t, "dumb_down_definition.html")
 
 	got, err := scraper.ParsePage(html)
 
@@ -43,4 +32,30 @@ func TestReadPageContent(t *testing.T) {
 		t.Errorf("expected %v but got %v", want, got)
 	}
 
+}
+
+func getParsedHTML(t testing.TB, filename string) *html.Node {
+	t.Helper()
+
+	reader := getFileContent(filename, t)
+
+	html, err := htmlquery.Parse(reader)
+	if err != nil {
+		t.Fatal("could not parse html")
+	}
+
+	return html
+}
+
+func getFileContent(filename string, t testing.TB) *strings.Reader {
+	t.Helper()
+
+	path := filepath.Join("testdata", filename)
+	content, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		t.Fatalf("error reading testfile %s: %s", filename, err)
+	}
+
+	return strings.NewReader(string(content))
 }
